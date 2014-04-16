29cbd3908d57f2ddc545c8be5f94dc8844b66411
Andy Portmen <andy.portmen@gmail.com>
2014-04-16 16:49:36 +0430

    compatibility fixes for Firefox 29

--- defaults/preferences/pref.js
+++ defaults/preferences/pref.js
@@ -1,4 +1,3 @@
-pref("extensions.fontfinder@bendodson.com.statusbar", true);
 pref("extensions.fontfinder@bendodson.com.contextmenu", true);
 pref("extensions.fontfinder@bendodson.com.statusBarLeftClick", false);
 pref("extensions.fontfinder@bendodson.com.analyzeLeftClick", false);
